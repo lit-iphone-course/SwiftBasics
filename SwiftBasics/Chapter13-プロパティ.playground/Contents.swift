eab28f241b81efddb47c35cb487daf85f60c9f4a
txxxxc <tomoya113.code@gmail.com>
2023-02-13 00:45:07 +0900

    [update] questions

--- SwiftBasics/Chapter13-プロパティ.playground/Contents.swift
+++ SwiftBasics/Chapter13-プロパティ.playground/Contents.swift
@@ -1,17 +1,22 @@
 /* 13.1 Chapter12で定義したMentorクラスに以下のプロパティを追加してください
     1. firstName
+        種類：変数
         型：String
         初期値: ""
     2. lastName
+        種類：変数
         型：String
         初期値: ""
     3. mentorName
+        種類：変数
         型：String
         初期値: ""
     4. course
+        種類：変数
         型：String
         初期値: ""
     4. season(期、例：12, 14など)
+        種類：変数
         型：Int
         初期値: 0
  
