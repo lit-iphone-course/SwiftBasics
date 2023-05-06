d0a2a637a35481102dab13cd75865a3fad38848e
txxxxc <tomoya113.code@gmail.com>
2023-05-06 18:37:11 +0900

    [update] SwiftBasics

--- SwiftBasics/Chapter10-繰り返し処理.playground/Contents.swift
+++
@@ -1,4 +0,0 @@
-// 10.1 for文を使って配列coursesに格納されている値をコンソールに出力してください
-var courses = ["iPhone", "Android", "WebD", "WebS", "Movie"]
-
-// 10.2 レンジとfor文を使って1から10の値をコンソールに出力してください
--- SwiftBasics/Chapter15-イニシャライザ.playground/Contents.swift
+++
@@ -1,2 +0,0 @@
-// 15.1 Chapter14で定義したMentorクラスにfistName, lastName, mentorName, course, seasonを受け取るイニシャライザを実装してください
-
