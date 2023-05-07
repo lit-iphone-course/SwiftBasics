c5ca7baf1a5d0c2b5f704eb4d3bd478b70b69d5b
txxxxc <tomoya113.code@gmail.com>
2023-05-07 17:21:10 +0900

    [update] SwiftBasics

--- SwiftBasics/[任意]Chapter10-Optional型.playground/Contents.swift
+++ SwiftBasics/[任意]Chapter10-Optional型.playground/Contents.swift
@@ -1,8 +1,11 @@
-// 10.1 optionalのInt型を定義してprintしてください
+// 10.1 optionalのInt型の変数(変数名optionalInt)を定義してprintしてください。初期値はnilにしてください。
 
 
-// 10.2 unwrappedNumberがnilの場合に初期値が10になるようにして、printしてください
+// 10.2 ??を使ってoptionalIntがnilの場合に初期値10を指定してprintしてください
 
 
-// 10.3 if let文を使って　アンラップしてprintしてください
+// 10.3 optionalIntに8を代入してください。
+
+
+// 10.4 if let文を使ってoptinalIntをアンラップしてprintしてください
 
